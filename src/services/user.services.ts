940abe5e35336e09cd42e08842a23fafa6abd42d
GharbiALI <gharbialicontacts@gmail.com>
2026-05-19 19:59:20 +0100

    feat(service): implement user registration

---
+++ src/services/user.services.ts
@@ -0,0 +1,20 @@
+import { createUser,findUserByEmail } from "../repository/user.repository";
+import bcrypt from "bcryptjs";
+
+export const registerUser = async (
+  name: string,
+  email: string,
+  password: string,
+) => {
+  const hashedPassword = await bcrypt.hash(password, 12);
+  const user = await createUser({
+    name,
+    email,
+    password: hashedPassword,
+  });
+  return user;
+};
+
+export const getUserByEmail = async (email: string) => {
+  return findUserByEmail(email); 
+}

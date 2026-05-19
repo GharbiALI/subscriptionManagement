a71b81062d1d059bc3b2aeb93e2a6f9f8c38d3b4
GharbiALI <gharbialicontacts@gmail.com>
2026-05-19 19:53:04 +0100

    feat(auth): implement jwt token generation

---
+++ src/auth/auth.services.ts
@@ -0,0 +1,19 @@
+import jwt from "jsonwebtoken";
+import { Types } from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const jwtSecret = process.env.JWT_SECRET as string;
+const jwtExpiresIn = process.env.jwtExpiresIn;
+
+export interface JwtPayload {
+  userId: string;
+  role: string;
+}
+
+export const generateToken = (userId: Types.ObjectId, role: string): string => {
+  return jwt.sign({ userId: userId.toString(), role }, jwtSecret, {
+    expiresIn: jwtExpiresIn,
+  } as jwt.SignOptions);
+};

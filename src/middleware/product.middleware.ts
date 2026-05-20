1b9fa87f3b86d21e03a4d93152cab3c0c00d55a9
GharbiALI <gharbialicontacts@gmail.com>
2026-05-20 13:58:45 +0100

    feat(middleware): implement product update body validation middleware

--- src/middleware/product.middleware.ts
+++ src/middleware/product.middleware.ts
@@ -1,5 +1,9 @@
 import { Request, Response, NextFunction } from "express";
-import { validateProductInput } from "../validator/product.validator";
+import {
+  validateProductInput,
+  validateProductUpdateInput,
+  isvalidateId
+} from "../validator/product.validator";
 
 export const validateProductMiddleware = (
   req: Request,
@@ -20,5 +24,52 @@ export const validateProductMiddleware = (
     return;
   }
 
+  next();
+};
+
+export const validateUpdateProductBody = (
+  req: Request,
+  res: Response,
+  next: NextFunction,
+): void => {
+  const { name, companyName, price, description, isActive } = req.body;
+
+  if (
+    name === undefined &&
+    companyName === undefined &&
+    price === undefined &&
+    description === undefined &&
+    isActive === undefined
+  ) {
+    res.status(400).json({ error: "At least one field is required to update" });
+    return;
+  }
+
+  const error = validateProductUpdateInput(
+    name,
+    companyName,
+    price,
+    description,
+    isActive,
+  );
+
+  if (error) {
+     res.status(400).json({ error });
+   
+  }
+
+  next();
+};
+
+export const validateIdMiddleware = (
+  req: Request,
+  res: Response,
+  next: NextFunction,
+): void => {
+  const { id } = req.params;
+ if(isvalidateId(id)) {
+    res.status(400).json({ error: "Invalid product ID" });
+    return; 
+  }
   next();
 };

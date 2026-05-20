6c56dbdf9018f6620f438bc7149ce9e8e7319aab
GharbiALI <gharbialicontacts@gmail.com>
2026-05-21 00:37:47 +0100

    feat(routes): register subscription routes in main router

--- src/routes/index.routes.ts
+++ src/routes/index.routes.ts
@@ -1,10 +1,12 @@
 import { Router } from "express";
 import usersRoutes from "./user.routes";
 import productRoutes from "./product.routes";
+import subscriptionRoutes from "./subscription.routes";
 
 const router = Router();
 
 router.use("/user",usersRoutes);
 router.use("/product", productRoutes);
+router.use("/subscription",subscriptionRoutes);
 
 export default router;

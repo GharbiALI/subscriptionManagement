e24974c4e2b916e78511a2c8dadea03c1c142c05
GharbiALI <gharbialicontacts@gmail.com>
2026-05-21 00:36:21 +0100

    feat(subscription): create subscription response data mapper interface

---
+++ src/mapper/subscription.mapper.ts
@@ -0,0 +1,33 @@
+import { ISubscription } from '../schemas/subscription.schemas';
+
+export interface SubscriptionResponse {
+  id: string;
+  userId: string;
+  product: {
+    id: string;
+    name: string;
+    companyName: string;
+    price: number;
+  };
+  startDate: Date;
+  expiryDate: Date;
+  status: 'active' | 'expired' | 'cancelled';
+}
+
+export const mapSubscription = (
+  subscription: ISubscription & { _id: any; productId: any },
+): SubscriptionResponse => {
+  return {
+    id: subscription._id.toString(),
+    userId: subscription.userId.toString(),
+    product: {
+      id: subscription.productId._id.toString(),
+      name: subscription.productId.name,
+      companyName: subscription.productId.companyName,
+      price: subscription.productId.price,
+    },
+    startDate: subscription.startDate,
+    expiryDate: subscription.expiryDate,
+    status: subscription.status,
+  };
+};

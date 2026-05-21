72de83d4d7d7aded85f118efeb770b208de46e64
GharbiALI <gharbialicontacts@gmail.com>
2026-05-21 19:04:46 +0100

    feat(controller): add cancelSub METHODE

--- src/controller/subscription.controller.ts
+++ src/controller/subscription.controller.ts
@@ -1,8 +1,8 @@
 import { Response, NextFunction } from "express";
-import { subscribe,getActiveSubscriptions,getExpiredSubscriptions } from "../services/subscription.services";
+import { subscribe,calculateHoursUntilExpiry,getActiveSubscriptions,getExpiredSubscriptions,cancelSubscription } from "../services/subscription.services";
 import { Product } from "../schemas/product.schemas";
 import { AuthRequest } from "../auth/auth.middleware";
-import { findActiveSubscriptionByUserAndProduct } from "../repository/subscription.repository";
+import { findActiveSubscriptionByUserAndProduct,findSubscriptionById } from "../repository/subscription.repository";
 
 export const createSubscription = async (
   req: AuthRequest,
@@ -58,6 +58,38 @@ export const getExpiredSub = async (
     const userId = req.user!.userId;
     const subscriptions = await getExpiredSubscriptions(userId);
     res.status(200).json({data: subscriptions });
+  } catch (err) {
+    next(err);
+  }
+};
+
+export const cancelSub = async (
+  req: AuthRequest,
+  res: Response,
+  next: NextFunction,
+): Promise<void> => {
+  try {
+    const { id } = req.params;
+    const userId = req.user!.userId;
+    const sub = await findSubscriptionById(id);
+    if (!sub) {
+      res.status(404).json({ error: "Subscription not found" });
+      return;
+    };
+    if (sub.userId.toString() !== userId) {
+      res.status(403).json({ error: "You can only cancel your own subscriptions" });
+      return;
+    };
+    if(sub.status !== 'active') {
+      res.status(400).json({ error: "Only active subscriptions can be cancelled" });
+      return;
+    };
+    if(calculateHoursUntilExpiry(sub.expiryDate) <= 48) {
+      res.status(400).json({ error: "Cancellation is only allowed more than 48 hours before the renewal date" });
+      return;
+    }
+    const result = await cancelSubscription(id);
+    res.status(200).json({ success: true, data: result });
   } catch (err) {
     next(err);
   }

9237a7f97aaed782ac15f9effd15ff198de2cfa7
GharbiALI <gharbialicontacts@gmail.com>
2026-05-29 10:04:54 +0100

    fix: correct database configuration

--- src/config/db.config.ts
+++ src/config/db.config.ts
@@ -3,22 +3,21 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-const MONGO_URI = process.env.MONGO_URI as string;
-const connectDB = async (): Promise<void> => {
+export default async function connectDB(): Promise<void> {
   try {
-    const conn = await mongoose.connect(MONGO_URI);
+    const MONGO_URI = process.env.MONGO_URI;
 
-    console.log(`Successfully connected to MongoDB: ${conn.connection.host} `);
-  } catch (error) {
-    if (error instanceof Error) {
-      console.error(`Error connecting to MongoDB: ${error.message} `);
-    } else {
-      console.error(
-        "An unknown error occurred while connecting to the database.",
-      );
+    if (!MONGO_URI) {
+      throw new Error("MONGO_URI is missing in .env file");
     }
+
+    await mongoose.connect(MONGO_URI);
+
+    console.log("MongoDB connected successfully");
+  } catch (err: any) {
+    console.error("Database connection failed:");
+    console.error(err.message);
+
     process.exit(1);
   }
-};
-
-export default connectDB;
+}

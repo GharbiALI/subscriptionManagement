377a39f5231338a236c258c5f2c791f663aae477
GharbiALI <gharbialicontacts@gmail.com>
2026-05-29 10:47:50 +0100

    refactor:  isolating profile endpoint

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -1,14 +1,10 @@
 import { Router } from "express";
-import { register, login, profile } from "../controller/user.controller";
+import { profile } from "../controller/user.controller";
 import { authenticate } from "../auth/auth.middleware";
-import {
-  validateUserMiddleware,
-  validateUserLoginMiddleware,
-} from "../middleware/user.middleware";
-const router = Router();
 
-router.post("/register", validateUserMiddleware, register);
-router.post("/login", validateUserLoginMiddleware, login);
-router.get("/profile", authenticate, profile);
+const router = Router();
+router.use(authenticate);
+
+router.get("/profile", profile);
 
 export default router;
